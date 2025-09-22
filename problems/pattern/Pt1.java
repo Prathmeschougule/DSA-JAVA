dd2982d0ad82dfc425af27475a286468587642d2
Prathmesh Chougule <prathmeshuix@gmail.com>
2025-09-23 00:08:18 +0530

    constructor oop Object Class abstract class interface Genrics LambdaExpression exception Handling and Collection Freamwork Enum , Vector , hashset , treeset pattern problem

---
+++ problems/pattern/Pt1.java
@@ -0,0 +1,27 @@
+package problems.pattern;
+
+//Number-Increasing Pyramid Pattern
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
+//1 2 3 4 5 6
+
+public class Pt1 {
+    public static void main(String[] args) {
+         int i , j ;
+
+         for (i= 1 ; i<=6 ; i++){
+
+             for (j=1; j<=i ; j++){
+
+                 System.out.print(j + " ");
+             }
+             System.out.println();
+         }
+
+
+
+    }
+}
---
+++ problems/pattern/Pt4.java
@@ -0,0 +1,32 @@
+package problems.pattern;
+
+//Zero-One Triangle Pattern
+//1
+//0 1
+//1 0 1
+//0 1 0 1
+//1 0 1 0 1
+//0 1 0 1 0 1
+
+
+public class Pt4 {
+    public static void main(String[] args) {
+
+        int i , j ;
+
+        for (i = 1 ; i <= 6; i++){
+
+            for (j = 1; j <= i ; j++){
+
+                int n = i+j;
+
+                if (n % 2 == 0){
+                    System.out.print( 1 + " ");
+                } else {
+                    System.out.print( 0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
